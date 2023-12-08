99a36fb1f9f0dc443941d1bd0b7de5192a3b3bf2
Vasia <rikomotoyev2018@gmail.com>
2023-12-08 10:40:06 +0200

    lab 8

--- cafe2/cafe.py
+++ cafe2/cafe.py
@@ -1,67 +1,52 @@
 import random
+import threading
+import time
 
 class EnhancedCafe:
-    def __init__(self, num_tables, num_waiters):
-        self.num_tables = num_tables
-        self.num_waiters = num_waiters
-        self.tables = [None] * num_tables
-        self.waiters = [0] * num_waiters
-        self.total_customers = 0
-        self.lost_customers = 0
-        self.waiting_customers = 0
+    # ... (попередній код класу EnhancedCafe)
 
-    def new_customer(self, current_time, peak_hours):
-        
-        # Шанс приходу клієнта змінюється в залежності від часу дня
-        
-        if random.random() < (0.2 if peak_hours else 0.05):
-            
-            # Якщо всі столи зайняті, клієнт чекає
-            if None not in self.tables:
-                self.waiting_customers += 1
-                if self.waiting_customers > 5: 
-                    self.lost_customers += 1
-                    self.waiting_customers -= 1
-            else:
-                table_index = self.tables.index(None)
-                self.tables[table_index] = current_time
-                self.total_customers += 1
-                self.waiting_customers = max(0, self.waiting_customers - 1)
+    def get_statistics(self):
+        # Метод для отримання статистики (наприклад, середній час очікування)
+        pass
 
-                # Призначення офіціанта
-                
-                available_waiter = self.waiters.index(min(self.waiters))
-                self.waiters[available_waiter] = current_time + random.randint(5, 15)
+def waiter_thread(cafe, waiter_id, lock):
+    # Офіціант обслуговує столики
+    while True:
+        with lock:
+            # Логіка для офіціанта
+            pass
+        time.sleep(random.uniform(1, 3))  # Відпочинок офіціанта
 
-    def update_tables(self, current_time):
-        
-        # Звільнення столів
-        
-        for i in range(len(self.tables)):
-            if self.tables[i] is not None:
-                if current_time - self.tables[i] > random.randint(30, 60):
-                    self.tables[i] = None
+def cafe_simulation(cafe, duration, lock):
+    end_time = time.time() + duration
+    while time.time() < end_time:
+        with lock:
+            # Оновлення стану кафе
+            pass
+        time.sleep(1)
+
+    # Після завершення симуляції
+    print("Симуляція завершена")
+    print(cafe.get_statistics())
 
 def simulate_enhanced_cafe(hours=12, num_tables=10, num_waiters=5):
     cafe = EnhancedCafe(num_tables, num_waiters)
-    for hour in range(hours):
-        peak_hours = 11 <= hour < 14 or 18 <= hour < 21  
-        for minute in range(60):
-            current_time = hour * 60 + minute
-            cafe.new_customer(current_time, peak_hours)
-            cafe.update_tables(current_time)
+    lock = threading.Lock()
+
+    threads = []
+    for _ in range(num_waiters):
+        t = threading.Thread(target=waiter_thread, args=(cafe, _, lock))
+        t.start()
+        threads.append(t)
+
+    simulation_thread = threading.Thread(target=cafe_simulation, args=(cafe, hours * 3600, lock))
+    simulation_thread.start()
+
+    for t in threads:
+        t.join()
+    simulation_thread.join()
 
     return cafe
 
-# Тестування симуляції з різною кількістю столиків та офіціантів
-
-results = []
-for tables in range(5, 21, 5):
-    for waiters in range(1, 6):
-        simulation = simulate_enhanced_cafe(num_tables=tables, num_waiters=waiters)
-        results.append((tables, waiters, simulation.total_customers, simulation.lost_customers))
-
-# Вивід результатів
-
-results
-
+# Використання симуляції
+simulate_enhanced_cafe()
