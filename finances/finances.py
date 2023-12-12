6b6cd1695b87f0da31be567d11d86d84cf509321
Vasia <rikomotoyev2018@gmail.com>
2023-12-12 11:52:37 +0200

    task + 2.0

--- finances/finances.py
+++ finances/finances.py
@@ -17,41 +17,46 @@ class TransactionTracker:
         self.balance = 0.0
 
     def add_transaction(self, transaction: Transaction):
+        if transaction.amount < 0:
+            print(f"Витрата: {transaction}")
+        else:
+            print(f"Дохід: {transaction}")
+
         self.transactions.append(transaction)
-        self.balance += transaction.amount
+        self.balance += transaction.amount  
 
     def remove_transaction(self, index: int):
         if 0 <= index < len(self.transactions):
             self.balance -= self.transactions[index].amount
             del self.transactions[index]
         else:
-            print("No transaction found at the given index.")
+            print("Не знайденно жодної транзакції")
 
     def get_balance(self):
         return self.balance
 
     def save_transactions_to_csv(self, file_path: str):
         try:
-            with open(file_path, mode='w', newline='') as file:
+            with open(file_path, mode='w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
-                writer.writerow(['Date', 'Amount', 'Description'])
+                writer.writerow(['Дата', 'Сцма', 'Опис'])
                 for transaction in self.transactions:
                     writer.writerow([transaction.date, transaction.amount, transaction.description])
-            print(f"Transactions saved to {file_path}")
+            print(f"Транзакція збережена до {file_path}")
         except Exception as e:
-            print(f"An error occurred while writing to the file: {e}")
+            print(f"Сталася помилка під час запису {e}")
 
 
 tracker = TransactionTracker()
 
 
-tracker.add_transaction(Transaction(100.0, 'Salary', datetime.date.today()))
-tracker.add_transaction(Transaction(-50.0, 'Groceries', datetime.date.today()))
+tracker.add_transaction(Transaction(100.0, 'Зарплата', datetime.date.today()))
+tracker.add_transaction(Transaction(-50.0, 'Продукти', datetime.date.today()))
 
 
 tracker.remove_transaction(0)
 
-print(f"Current balance: {tracker.get_balance()}")
+print(f"Поточний баланс: {tracker.get_balance()}")
 
 
 tracker.save_transactions_to_csv('transactions.csv')
